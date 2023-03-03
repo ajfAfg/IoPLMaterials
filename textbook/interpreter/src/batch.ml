baadec2e1a9ad3c6b719bd9b3b7118d53c2bea24
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-03-03 21:31:47 +0900

    use the new parser

--- textbook/interpreter/src/batch.ml
+++ textbook/interpreter/src/batch.ml
@@ -4,12 +4,11 @@ let rec eval_print env lexbuf =
   (* NOTE:
      `Lexer.main` seems to execute `exit 0`
      when a certain string (an unacceptble string?; ex. "\n") is inputted. *)
-  let program = Parser.toplevel Lexer.main lexbuf in
-  let defs, newenv = eval_program env program in
+  let program = Parser.items Lexer.main lexbuf in
+  let defs, _newenv = eval_program env program in
   List.iter
     (fun (id, v) -> Printf.printf "val %s = %s\n" id (string_of_exval v))
-    defs;
-  eval_print newenv lexbuf
+    defs
 
 (* NOTE: No exception handling to perform the same behavior as the original *)
 let read_eval_print filename =
