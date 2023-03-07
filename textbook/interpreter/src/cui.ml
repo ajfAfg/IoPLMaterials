0f8a8aba70b99c4abfc76b45dec49fae805446e4
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-03-07 23:35:16 +0900

    use the module `Run` instead of `PrettyEval`

--- textbook/interpreter/src/cui.ml
+++ textbook/interpreter/src/cui.ml
@@ -1,16 +1,16 @@
-let rec read_eval_print' env =
+let rec read_eval_print' env tyenv =
   print_string "# ";
   flush stdout;
   try
     let program =
       Parser.toplevel_input Lexer.main (Lexing.from_channel stdin)
     in
-    let results, newenv = PrettyEval.eval_program env program in
-    print_endline @@ PrettyEval.string_of_results results;
-    read_eval_print' newenv
+    let results, env', tyenv' = Run.run_program env tyenv program in
+    print_endline @@ Run.string_of_run_results results;
+    read_eval_print' env' tyenv'
   with exn ->
     Printf.printf "Fatal error: exception %s\n" (Printexc.to_string exn);
-    read_eval_print' env
+    read_eval_print' env tyenv
 
 let initial_program =
   let open Syntax in
@@ -23,7 +23,25 @@ let initial_program =
     Def [ ("iv", ILit 4) ];
   ]
 
+(* TODO:
+   When type inference of variable definitions becomes possible,
+   there is no need to declare types explicitly. *)
+let initial_tyenv =
+  let open Syntax in
+  List.fold_left
+    (fun tyenv (id, ty) -> Environment.extend id ty tyenv)
+    Environment.empty
+    [
+      ("x", TyInt);
+      ("v", TyInt);
+      ("i", TyInt);
+      ("ii", TyInt);
+      ("iii", TyInt);
+      ("iv", TyInt);
+    ]
+
 let read_eval_print () =
-  MyStdlib.program @ initial_program
-  |> Eval.eval_program Environment.empty
-  |> read_eval_print'
+  let env =
+    MyStdlib.program @ initial_program |> Eval.eval_program Environment.empty
+  in
+  read_eval_print' env initial_tyenv
